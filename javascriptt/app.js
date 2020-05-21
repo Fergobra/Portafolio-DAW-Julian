e114e9daec46ae039325548615e2adbee5960893
Fergobra <fergobra.5994@gmail.com>
2020-05-21 16:14:46 -0500

    Primera
    Cargando archivos

---
+++ javascriptt/app.js
@@ -0,0 +1,63 @@
+var nombre = [];
+var numero = [];
+var correo = [];
+var direccion = [];
+var contactos = [];
+var i = 0;
+
+function agregar(){
+
+nombre[i] = prompt("Nombre nuevo contacto");
+numero[i] = prompt("Número de contacto")
+direccion[i] = prompt("Dirección de contacto");
+correo[i] = prompt("Email de contacto");
+contactos[i] = nombre[i] + " " 
++ " " + numero[i] + " " + correo[i]
++ " " + direccion[i];
+alert("Contacto Guardado: " + nombre +" " + numero +" "  + direccion +" "  + correo );
+return  i = i + 1;
+}
+
+function verlista(){
+    document.getElementById('mostrador').innerHTML = ("<h3>Lista de contactos</h3><hr>");
+	for (var i = contactos.length - 1; i >= 0; i--) {
+		document.getElementById('mostrador').innerHTML += (contactos[i]+"<br/><hr>");
+	}
+}
+
+function buscar(){
+    var buscarNom = prompt("Ingrese el nombre a buscar:");
+	var results=0;
+	document.getElementById('mostrador').innerHTML = ("<h3>Contacto encontrado</h3><br />");
+	for (var i = contactos.length - 1; i >= 0; i--) {
+		if(buscarNom==nombre[i]){
+			document.getElementById('mostrador').innerHTML += (contactos[i]+"<br/>");
+			results++;
+		}
+	}
+	if(results==0)
+		document.getElementById('mostrador').innerHTML += ("no tienes ningún contacto registrado con ese nombre");
+}
+function menu(){
+	var menu = prompt("Bienvenido al menú, oprime el número correspondiente a la opción deseada y presiona enter: 1. Añadir Contacto  /  2. Ver Lista de contactos  /  3. Buscar un contacto");
+	switch(menu){
+		case '1':
+		agregar();
+		break;
+
+		case '2':
+		verlista();
+		break;
+
+		case '3':
+		buscar()
+		break;
+
+		default:
+		document.getElementById('mostrador').innerHTML = ("Ha introducido una opcion invalida, por favor intentelo de nuevo");
+		break
+
+	}
+
+
+}
